f43ed6ee9d445b6b255f5678151e6e7ef80fa7d6
lelis <lelis685@gmail.com>
2022-10-30 13:34:15 -0300

    feat: Criando migrações e mapeando as entidades Pedido e ItemPedido

---
+++ src/main/java/com/algaworks/algafood/domain/model/ItemPedido.java
@@ -0,0 +1,34 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class ItemPedido {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private BigDecimal precoUnitario;
+	private BigDecimal precoTotal;
+	private Integer quantidade;
+	private String observacao;
+
+	@ManyToOne
+	@JoinColumn(name = "produto_id",nullable = false)
+	private Produto produto;
+
+	@ManyToOne
+	@JoinColumn(name = "pedido_id",nullable = false)
+	private Pedido pedido;
+
+}
