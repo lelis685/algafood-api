53638eb359090d37ccee51aaa89dc0688bbf0f1c
lelis <lelis685@gmail.com>
2022-11-01 19:01:34 -0300

    feat:  Criando constraints de validação customizadas com implementação de ConstraintValidator

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.domain.model;
 
 import com.algaworks.algafood.core.validation.Groups;
+import com.algaworks.algafood.core.validation.Multiplo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -34,6 +35,7 @@ public class Restaurante {
 
     @NotNull
     @PositiveOrZero
+    @Multiplo(numero = 5)
     @Column(name="taxa_frete")
     private BigDecimal taxaFrete;
 
