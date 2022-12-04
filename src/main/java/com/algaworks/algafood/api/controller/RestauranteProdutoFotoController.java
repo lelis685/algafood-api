b952fa976eb91460fabad65895fc502ef583800b
lelis <lelis685@gmail.com>
2022-12-04 11:56:02 -0300

    feat: implementando upload arquivo

---
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -0,0 +1,41 @@
+package com.algaworks.algafood.api.controller;
+
+import com.algaworks.algafood.api.model.input.FotoProdutoInput;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
+public class RestauranteProdutoFotoController {
+
+
+
+    @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void atualizarFoto(@PathVariable Long restauranteId,
+                              @PathVariable Long produtoId,
+                              @Valid FotoProdutoInput fotoProdutoInput){
+
+        var nomeArquivo = UUID.randomUUID().toString() + "_" +  fotoProdutoInput.getArquivo().getOriginalFilename();
+        var arquivoFoto = Path.of("C:/Users/Lelis/Pictures/catalogo", nomeArquivo); // TODO ajustar
+
+        System.out.println(arquivoFoto);
+        System.out.println(fotoProdutoInput.getArquivo().getContentType());
+        System.out.println(fotoProdutoInput.getDescricao());
+
+        try {
+            fotoProdutoInput.getArquivo().transferTo(arquivoFoto);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+
+}
