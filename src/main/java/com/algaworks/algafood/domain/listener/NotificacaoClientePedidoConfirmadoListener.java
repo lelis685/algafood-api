2d71a2ab663e3370ed18338053159e5aa99eeb06
lelis <lelis685@gmail.com>
2022-12-09 11:38:25 -0300

    feat: controlando momento envio email

--- src/main/java/com/algaworks/algafood/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
+++ src/main/java/com/algaworks/algafood/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
@@ -6,6 +6,8 @@ import com.algaworks.algafood.domain.service.EnvioEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 import static com.algaworks.algafood.domain.service.EnvioEmailService.*;
 
@@ -16,7 +18,7 @@ public class NotificacaoClientePedidoConfirmadoListener {
     private EnvioEmailService envioEmail;
 
 
-    @EventListener
+    @TransactionalEventListener
     public void aoConfirmarPedido(PedidoConfirmadoEvent event){
         Pedido pedido = event.getPedido();
 
