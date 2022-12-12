195ca7b9dcb6ad326b39ba4215e05803d7b25546
lelis <lelis685@gmail.com>
2022-12-12 20:50:24 -0300

    feat: documentando api cidades

--- src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -1,9 +1,23 @@
 package com.algaworks.algafood.api.controller;
 
-import java.util.List;
+import com.algaworks.algafood.api.assembler.CidadeInputDisassembler;
+import com.algaworks.algafood.api.assembler.CidadeModelAssembler;
+import com.algaworks.algafood.api.exceptionhandler.Problem;
+import com.algaworks.algafood.api.model.CidadeModel;
+import com.algaworks.algafood.api.model.input.CidadeInput;
+import com.algaworks.algafood.domain.exception.EstadoNaoEncontradoException;
+import com.algaworks.algafood.domain.exception.NegocioException;
+import com.algaworks.algafood.domain.model.Cidade;
+import com.algaworks.algafood.domain.repository.CidadeRepository;
+import com.algaworks.algafood.domain.service.CadastroCidadeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
-import javax.validation.Valid;
-
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,19 +30,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.algaworks.algafood.api.assembler.CidadeInputDisassembler;
-import com.algaworks.algafood.api.assembler.CidadeModelAssembler;
-import com.algaworks.algafood.api.model.CidadeModel;
-import com.algaworks.algafood.api.model.input.CidadeInput;
-import com.algaworks.algafood.domain.exception.EstadoNaoEncontradoException;
-import com.algaworks.algafood.domain.exception.NegocioException;
-import com.algaworks.algafood.domain.model.Cidade;
-import com.algaworks.algafood.domain.repository.CidadeRepository;
-import com.algaworks.algafood.domain.service.CadastroCidadeService;
-
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import javax.validation.Valid;
+import java.util.List;
 
 @Api(tags = "Cidades")
 @RestController
@@ -56,6 +59,12 @@ public class CidadeController {
 	}
 
 	@ApiOperation("Busca uma cidade por ID")
+	@ApiResponses({
+			@ApiResponse(responseCode = "400", description = "ID da cidade inválido",
+					content = @Content(schema = @Schema(implementation = Problem.class))),
+			@ApiResponse(responseCode = "404", description = "Cidade não encontrada",
+					content = @Content(schema = @Schema(implementation = Problem.class)))
+	})
 	@GetMapping("/{cidadeId}")
 	public CidadeModel buscar(
 			@ApiParam(value = "ID de uma cidade", example = "1")
@@ -66,6 +75,7 @@ public class CidadeController {
 	}
 
 	@ApiOperation("Cadastra uma cidade")
+	@ApiResponse(responseCode = "201", description = "Cidade cadastrada")
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public CidadeModel adicionar(
@@ -83,6 +93,11 @@ public class CidadeController {
 	}
 
 	@ApiOperation("Atualiza uma cidade por ID")
+	@ApiResponses({
+			@ApiResponse(responseCode = "200", description = "Cidade atualizada"),
+			@ApiResponse(responseCode = "404", description = "Cidade não encontrada",
+					content = @Content(schema = @Schema(implementation = Problem.class)))
+	})
 	@PutMapping("/{cidadeId}")
 	public CidadeModel atualizar(
 			@ApiParam(value = "ID de uma cidade", example = "1")
@@ -104,6 +119,11 @@ public class CidadeController {
 	}
 
 	@ApiOperation("Exclui uma cidade por ID")
+	@ApiResponses({
+			@ApiResponse(responseCode = "204", description = "Cidade excluída"),
+			@ApiResponse(responseCode = "404", description = "Cidade não encontrada",
+					content = @Content(schema = @Schema(implementation = Problem.class)))
+	})
 	@DeleteMapping("/{cidadeId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void remover(
--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -1,44 +1,127 @@
 package com.algaworks.algafood.core.openapi;
 
+import com.algaworks.algafood.api.exceptionhandler.Problem;
+import com.fasterxml.classmate.TypeResolver;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.RequestHandlerSelectors;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.*;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Response;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.json.JacksonModuleRegistrar;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
 
 @Configuration
-public class SpringFoxConfig implements WebMvcConfigurer {
+@Import(BeanValidatorPluginsConfiguration.class)
+public class SpringFoxConfig {
 
-  @Bean
-  public Docket apiDocket() {
-    return new Docket(DocumentationType.OAS_30)
-        .select()
-          .apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
-          .build()
-            .apiInfo(apiInfo())
-            .tags(new Tag("Cidades", "Gerencia as cidades"));
-  }
+    @Bean
+    public Docket apiDocket() {
 
-  public ApiInfo apiInfo(){
-    return new ApiInfoBuilder()
-            .title("Alafood API")
-            .description("API aberta para clientes e restaurantes")
-            .version("1")
-            .build();
-  }
+        var typeResolver = new TypeResolver();
 
-  @Override
-  public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("swagger-ui.html")
-            .addResourceLocations("classpath:/META-INF/resources/");
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
+                .paths(PathSelectors.any())
+                .build()
+                .useDefaultResponseMessages(false)
+                .globalResponses(HttpMethod.GET, globalGetResponseMessages())
+                .globalResponses(HttpMethod.POST, globalPostPutResponseMessages())
+                .globalResponses(HttpMethod.PUT, globalPostPutResponseMessages())
+                .globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())
+                .additionalModels(typeResolver.resolve(Problem.class))
+                .apiInfo(apiInfo())
+                .tags(new Tag("Cidades", "Gerencia as cidades"));
+    }
 
-    registry.addResourceHandler("/webjars/**")
-            .addResourceLocations("classpath:/META-INF/resources/webjars/");
-  }
+    @Bean
+    public JacksonModuleRegistrar springFoxJacksonConfig() {
+        return objectMapper -> objectMapper.registerModule(new JavaTimeModule());
+    }
+
+    private List<Response> globalGetResponseMessages() {
+        return Arrays.asList(
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+                        .description("Erro interno do Servidor")
+                        .representation(MediaType.APPLICATION_JSON)
+                        .apply(getProblemaModelReference())
+                        .build(),
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+                        .description("Recurso não possui representação que pode ser aceita pelo consumidor")
+                        .build()
+        );
+    }
+
+    private List<Response> globalPostPutResponseMessages() {
+        return Arrays.asList(
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+                        .description("Requisição inválida (erro do cliente)")
+                        .representation(MediaType.APPLICATION_JSON)
+                        .apply(getProblemaModelReference())
+                        .build(),
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+                        .description("Erro interno no servidor")
+                        .representation(MediaType.APPLICATION_JSON)
+                        .apply(getProblemaModelReference())
+                        .build(),
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+                        .description("Recurso não possui representação que poderia ser aceita pelo consumidor")
+                        .build(),
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()))
+                        .description("Requisição recusada porque o corpo está em um formato não suportado")
+                        .representation(MediaType.APPLICATION_JSON)
+                        .apply(getProblemaModelReference())
+                        .build()
+        );
+    }
+
+    private List<Response> globalDeleteResponseMessages() {
+        return Arrays.asList(
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+                        .description("Requisição inválida (erro do cliente)")
+                        .representation(MediaType.APPLICATION_JSON)
+                        .apply(getProblemaModelReference())
+                        .build(),
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+                        .description("Erro interno no servidor")
+                        .representation(MediaType.APPLICATION_JSON)
+                        .apply(getProblemaModelReference())
+                        .build()
+        );
+    }
+
+    private Consumer<RepresentationBuilder> getProblemaModelReference() {
+        return r -> r.model(m -> m.name("Problema")
+                .referenceModel(ref -> ref.key(k -> k.qualifiedModelName(
+                        q -> q.name("Problema").namespace("com.algaworks.algafood.api.exceptionhandler")))));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("AlgaFood API")
+                .description("API aberta para clientes e restaurantes")
+                .version("1")
+                .build();
+    }
+
 }
