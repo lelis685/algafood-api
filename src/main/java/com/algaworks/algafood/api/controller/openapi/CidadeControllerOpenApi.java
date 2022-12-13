d83a7b51d138c584bb2f6dcd302562500658a27b
lelis <lelis685@gmail.com>
2022-12-13 14:04:55 -0300

    feat: separando doc api cidades

--- src/main/java/com/algaworks/algafood/api/controller/openapi/CidadeControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/controller/openapi/CidadeControllerOpenApi.java
@@ -6,58 +6,50 @@ import com.algaworks.algafood.api.model.input.CidadeInput;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 import java.util.List;
 
 @Api(tags = "Cidades")
 public interface CidadeControllerOpenApi {
 
-
     @ApiOperation("Lista as cidades")
     public List<CidadeModel> listar();
 
-
     @ApiOperation("Busca uma cidade por ID")
     @ApiResponses({
-            @ApiResponse(responseCode = "400", description = "ID da cidade inválido",
-                    content = @Content(schema = @Schema(implementation = Problem.class))),
-            @ApiResponse(responseCode = "404", description = "Cidade não encontrada",
-                    content = @Content(schema = @Schema(implementation = Problem.class)))
+            @ApiResponse(code = 400, message = "ID da cidade inválido", response = Problem.class),
+            @ApiResponse(code = 404, message = "Cidade não encontrada", response = Problem.class)
     })
     public CidadeModel buscar(
             @ApiParam(value = "ID de uma cidade", example = "1")
             Long cidadeId);
 
-
     @ApiOperation("Cadastra uma cidade")
-    @ApiResponse(responseCode = "201", description = "Cidade cadastrada")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Cidade cadastrada"),
+    })
     public CidadeModel adicionar(
             @ApiParam(name = "corpo", value = "Representação de uma nova cidade")
             CidadeInput cidadeInput);
 
-
     @ApiOperation("Atualiza uma cidade por ID")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Cidade atualizada"),
-            @ApiResponse(responseCode = "404", description = "Cidade não encontrada",
-                    content = @Content(schema = @Schema(implementation = Problem.class)))
+            @ApiResponse(code = 200, message = "Cidade atualizada"),
+            @ApiResponse(code = 404, message = "Cidade não encontrada", response = Problem.class)
     })
     public CidadeModel atualizar(
             @ApiParam(value = "ID de uma cidade", example = "1")
             Long cidadeId,
+
             @ApiParam(name = "corpo", value = "Representação de uma cidade com os novos dados")
             CidadeInput cidadeInput);
 
-
     @ApiOperation("Exclui uma cidade por ID")
     @ApiResponses({
-            @ApiResponse(responseCode = "204", description = "Cidade excluída"),
-            @ApiResponse(responseCode = "404", description = "Cidade não encontrada",
-                    content = @Content(schema = @Schema(implementation = Problem.class)))
+            @ApiResponse(code = 204, message = "Cidade excluída"),
+            @ApiResponse(code = 404, message = "Cidade não encontrada", response = Problem.class)
     })
     public void remover(
             @ApiParam(value = "ID de uma cidade", example = "1")
