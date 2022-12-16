120af9ee904af8b1a846a3a91dfca2e3107c665d
lelis <lelis685@gmail.com>
2022-12-16 18:23:19 -0300

    feat: add TemplateVariables em pedidos

--- src/main/java/com/algaworks/algafood/api/assembler/PedidoModelAssembler.java
+++ src/main/java/com/algaworks/algafood/api/assembler/PedidoModelAssembler.java
@@ -1,11 +1,20 @@
 package com.algaworks.algafood.api.assembler;
 
-import com.algaworks.algafood.api.controller.*;
-import com.algaworks.algafood.api.model.*;
+import com.algaworks.algafood.api.controller.CidadeController;
+import com.algaworks.algafood.api.controller.FormaPagamentoController;
+import com.algaworks.algafood.api.controller.PedidoController;
+import com.algaworks.algafood.api.controller.RestauranteController;
+import com.algaworks.algafood.api.controller.RestauranteProdutoController;
+import com.algaworks.algafood.api.controller.UsuarioController;
+import com.algaworks.algafood.api.model.PedidoModel;
 import com.algaworks.algafood.domain.model.Pedido;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.TemplateVariable;
+import org.springframework.hateoas.TemplateVariable.VariableType;
+import org.springframework.hateoas.TemplateVariables;
+import org.springframework.hateoas.UriTemplate;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 
@@ -13,7 +22,8 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Component
-public class PedidoModelAssembler extends RepresentationModelAssemblerSupport<Pedido, PedidoModel> {
+public class PedidoModelAssembler
+        extends RepresentationModelAssemblerSupport<Pedido, PedidoModel> {
 
     @Autowired
     private ModelMapper modelMapper;
@@ -27,36 +37,41 @@ public class PedidoModelAssembler extends RepresentationModelAssemblerSupport<Pe
         PedidoModel pedidoModel = createModelWithId(pedido.getCodigo(), pedido);
         modelMapper.map(pedido, pedidoModel);
 
-        FormaPagamentoModel formaPagamentoModel = pedidoModel.getFormaPagamento();
-        UsuarioModel usuarioModel = pedidoModel.getCliente();
-        RestauranteResumoModel modelRestaurante = pedidoModel.getRestaurante();
-        CidadeResumoModel cidadeResumoModel = pedidoModel.getEnderecoEntrega().getCidade();
+        TemplateVariables pageVariables = new TemplateVariables(
+                new TemplateVariable("page", VariableType.REQUEST_PARAM),
+                new TemplateVariable("size", VariableType.REQUEST_PARAM),
+                new TemplateVariable("sort", VariableType.REQUEST_PARAM));
 
-        pedidoModel.add(linkTo(PedidoController.class).withRel("pedidos"));
+        TemplateVariables filtroVariables = new TemplateVariables(
+                new TemplateVariable("clienteId", VariableType.REQUEST_PARAM),
+                new TemplateVariable("restauranteId", VariableType.REQUEST_PARAM),
+                new TemplateVariable("dataCriacaoInicio", VariableType.REQUEST_PARAM),
+                new TemplateVariable("dataCriacaoFim", VariableType.REQUEST_PARAM));
 
-        formaPagamentoModel.add(linkTo(methodOn(FormaPagamentoController.class)
-                .buscar(formaPagamentoModel.getId(), null)).withSelfRel());
+        String pedidosUrl = linkTo(PedidoController.class).toUri().toString();
 
-        usuarioModel.add(linkTo(methodOn(UsuarioController.class)
-                .buscar(usuarioModel.getId())).withSelfRel());
+        pedidoModel.add(Link.of(UriTemplate.of(pedidosUrl,
+                pageVariables.concat(filtroVariables)), "pedidos"));
 
-        modelRestaurante.add(linkTo(methodOn(RestauranteController.class)
-                .buscar(modelRestaurante.getId())).withSelfRel());
+        pedidoModel.getRestaurante().add(linkTo(methodOn(RestauranteController.class)
+                .buscar(pedido.getRestaurante().getId())).withSelfRel());
 
-        cidadeResumoModel.add(linkTo(methodOn(CidadeController.class)
-                .buscar(cidadeResumoModel.getId())).withSelfRel());
+        pedidoModel.getCliente().add(linkTo(methodOn(UsuarioController.class)
+                .buscar(pedido.getCliente().getId())).withSelfRel());
+
+        pedidoModel.getFormaPagamento().add(linkTo(methodOn(FormaPagamentoController.class)
+                .buscar(pedido.getFormaPagamento().getId(), null)).withSelfRel());
+
+        pedidoModel.getEnderecoEntrega().getCidade().add(linkTo(methodOn(CidadeController.class)
+                .buscar(pedido.getEnderecoEntrega().getCidade().getId())).withSelfRel());
 
         pedidoModel.getItens().forEach(item -> {
             item.add(linkTo(methodOn(RestauranteProdutoController.class)
-                    .buscar(pedidoModel.getRestaurante().getId(), item.getProdutoId())).withRel("produto"));
+                    .buscar(pedidoModel.getRestaurante().getId(), item.getProdutoId()))
+                    .withRel("produto"));
         });
 
         return pedidoModel;
     }
 
-    @Override
-    public CollectionModel<PedidoModel> toCollectionModel(Iterable<? extends Pedido> entities) {
-        return super.toCollectionModel(entities)
-                .add(linkTo(PedidoController.class).withSelfRel());
-    }
 }
