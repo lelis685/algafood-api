fd4c4b5ef4f6c020bb1318cacdd9b2e0ad4ef083
lelis <lelis685@gmail.com>
2022-12-19 12:12:31 -0300

    feat: iniciando v2 para api cidades

---
+++ src/main/java/com/algaworks/algafood/api/v2/controller/CidadeControllerV2.java
@@ -0,0 +1,96 @@
+package com.algaworks.algafood.api.v2.controller;
+
+import com.algaworks.algafood.api.ResourceUriHelper;
+import com.algaworks.algafood.api.v1.assembler.CidadeInputDisassembler;
+import com.algaworks.algafood.api.v1.assembler.CidadeModelAssembler;
+import com.algaworks.algafood.api.v2.assembler.CidadeInputDisassemblerV2;
+import com.algaworks.algafood.api.v2.assembler.CidadeModelAssemblerV2;
+import com.algaworks.algafood.api.v2.model.CidadeModelV2;
+import com.algaworks.algafood.api.v1.model.input.CidadeInput;
+import com.algaworks.algafood.api.v1.openapi.controller.CidadeControllerOpenApi;
+import com.algaworks.algafood.api.v2.model.input.CidadeInputV2;
+import com.algaworks.algafood.core.web.AlgaMediaTypes;
+import com.algaworks.algafood.domain.exception.EstadoNaoEncontradoException;
+import com.algaworks.algafood.domain.exception.NegocioException;
+import com.algaworks.algafood.domain.model.Cidade;
+import com.algaworks.algafood.domain.repository.CidadeRepository;
+import com.algaworks.algafood.domain.service.CadastroCidadeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/cidades")
+public class CidadeControllerV2 {
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
+	@Autowired
+	private CadastroCidadeService cadastroCidade;
+
+	@Autowired
+	private CidadeModelAssemblerV2 cidadeModelAssembler;
+
+	@Autowired
+	private CidadeInputDisassemblerV2 cidadeInputDisassembler;
+
+	@GetMapping(produces = AlgaMediaTypes.V2_APPLICATION_JSON_VALUE)
+	public CollectionModel<CidadeModelV2> listar() {
+		List<Cidade> todasCidades = cidadeRepository.findAll();
+
+		return cidadeModelAssembler.toCollectionModel(todasCidades);
+	}
+
+	@GetMapping(path = "/{cidadeId}", produces = AlgaMediaTypes.V2_APPLICATION_JSON_VALUE)
+	public CidadeModelV2 buscar(@PathVariable Long cidadeId) {
+		Cidade cidade = cadastroCidade.buscarOuFalhar(cidadeId);
+
+		return cidadeModelAssembler.toModel(cidade);
+	}
+
+	@PostMapping(produces = AlgaMediaTypes.V2_APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.CREATED)
+	public CidadeModelV2 adicionar(@RequestBody @Valid CidadeInputV2 cidadeInput) {
+		try {
+			Cidade cidade = cidadeInputDisassembler.toDomainObject(cidadeInput);
+
+			cidade = cadastroCidade.salvar(cidade);
+
+			CidadeModelV2 cidadeModel = cidadeModelAssembler.toModel(cidade);
+
+			ResourceUriHelper.addUriInResponseHeader(cidadeModel.getIdCidade());
+
+			return cidadeModel;
+		} catch (EstadoNaoEncontradoException e) {
+			throw new NegocioException(e.getMessage(), e);
+		}
+	}
+
+	@PutMapping(path = "/{cidadeId}", produces = AlgaMediaTypes.V2_APPLICATION_JSON_VALUE)
+	public CidadeModelV2 atualizar(@PathVariable Long cidadeId,
+	                             @RequestBody @Valid CidadeInputV2 cidadeInput) {
+		try {
+			Cidade cidadeAtual = cadastroCidade.buscarOuFalhar(cidadeId);
+
+			cidadeInputDisassembler.copyToDomainObject(cidadeInput, cidadeAtual);
+
+			cidadeAtual = cadastroCidade.salvar(cidadeAtual);
+
+			return cidadeModelAssembler.toModel(cidadeAtual);
+		} catch (EstadoNaoEncontradoException e) {
+			throw new NegocioException(e.getMessage(), e);
+		}
+	}
+
+	@DeleteMapping(path = "/{cidadeId}", produces = AlgaMediaTypes.V2_APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long cidadeId) {
+		cadastroCidade.excluir(cidadeId);
+	}
+
+}
