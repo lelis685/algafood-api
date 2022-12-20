66fc932cd544ff4e0495dbbc4676da34447b8525
lelis <lelis685@gmail.com>
2022-12-20 09:30:56 -0300

    feat: add loggin

--- src/main/java/com/algaworks/algafood/api/v1/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/v1/controller/CozinhaController.java
@@ -8,6 +8,9 @@ import com.algaworks.algafood.api.v1.openapi.controller.CozinhaControllerOpenApi
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import com.algaworks.algafood.domain.service.CadastroCozinhaService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -28,6 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/v1/cozinhas")
 public class CozinhaController implements CozinhaControllerOpenApi {
@@ -50,6 +54,8 @@ public class CozinhaController implements CozinhaControllerOpenApi {
 	@Override
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public PagedModel<CozinhaModel> listar(@PageableDefault(size = 10) Pageable pageable) {
+		log.info("Listando cozinhas com páginas de {} registors...", pageable.getPageSize());
+
 		Page<Cozinha> cozinhasPage = cozinhaRepository.findAll(pageable);
 
 		PagedModel<CozinhaModel> cozinhasPagedModel = pagedResourcesAssembler
